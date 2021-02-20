ccd9ec673ce208307ffb5ba799cb8371ed67f0d9
Evan Magnussen <magnussen.evan@gmail.com>
2021-02-20 12:38:02 -0500

    logging and strip prefix default handler

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 	"path"
@@ -17,17 +18,20 @@ import (
 // this is what runs on 'startup' once the server is installed and running
 // the daemon can be interfaced with using the `broc` command
 func main() {
+	log.Println("Loading Config")
 	config, err := internals.LoadConfig("./broccoli.config.yaml")
 	if err != nil {
 		panic(err)
 	}
 
+	log.Println("Loading Plugins")
 	mapping := loadPluginsToRoutes(config.PluginsDir)
 
 	// This is the defualt handler, It allows us to handle relative proxied paths that
 	// are absolute in the response... hacky and need to do research on how to improve.
 	// TODO: handle recursive searches of referrer paths, this is bound to be a buggy
 	//       piece of code to handle.
+	log.Println("Loading default route '/'")
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		u, _ := url.Parse(req.Referer())
 
@@ -41,7 +45,7 @@ func main() {
 
 	go api.InitApi(config.AdminPort, mapping)
 
-	http.ListenAndServe(fmt.Sprintf(":%d", config.GatewayPort), nil)
+	log.Fatalf("Server Error: %s", http.ListenAndServe(fmt.Sprintf(":%d", config.GatewayPort), nil))
 }
 
 // consumes a config pointer and returns a map of urls that point to the route
@@ -55,13 +59,15 @@ func loadPluginsToRoutes(pluginsDir string) map[string]*internals.Route {
 	serviceMap := map[string]*internals.Route{}
 
 	for _, file := range matches {
+		log.Printf("Found Plugin %s", file)
 		plg, err := plugin.Open(file)
 		if err != nil {
 			panic(err)
 		}
 		route := internals.NewRouteFromPlugin(plg)
 
-		http.Handle(route.Uri, route)
+		log.Printf("Setting Route '%s' Proxy To '%s'", route.Uri, route.Upstream.Host)
+		http.Handle(route.Uri, http.StripPrefix(route.Uri, route))
 		serviceMap[route.Uri] = route
 	}
 
